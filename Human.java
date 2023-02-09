72ae97db1ce691b72c81fe7536438f30020dee38
Andres Cordova <andres.cordova@telusinternational.com>
2023-02-09 17:41:22 -0600

    fixing some warnings

--- Human.java
+++ Human.java
@@ -5,13 +5,20 @@ public class Human extends Player {
     public Human(String name, int age, int id) {
         super(name, age, id);
     }
-    
-    @Override
-    public int getOption(){
 
+    @Override
+    public int getOption() {
         int option = 0;
-        Scanner scaner = new Scanner(System.in);
-        option = scaner.nextInt();
+        Scanner scaner = null;
+        try {
+            scaner = new Scanner(System.in);
+            option = scaner.nextInt();
+
+        } catch (Exception e) {
+            if (scaner != null) {
+                scaner.close();
+            }
+        }
         return option;
     }
 }
